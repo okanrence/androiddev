ac02cded47a6b50f6e6d47a5613efe576a6cdf99
olanrewaju <olanrewaju.okanrende@fidelitybank.ng>
2017-02-22 09:41:34 +0100

    Add project files.

---
+++ SchoolApp.Android/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace SchoolApp.Android
+{
+    [Activity(Label = "SchoolApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+             SetContentView (Resource.Layout.Main);
+
+        }
+    }
+}
+
---
+++ SchoolApp.Core/StudentsService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchoolApp.Core
+{
+    public class StudentsService
+    {
+        public List<Student> GetStudents()
+        {
+            return new List<Student> { new Student { Name = "Olanrewaju", Height = 123, StudentID = 1 }, new Student { Name = "Frank", Height = 123, StudentID = 2 } };
+        }
+    }
+
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+}
